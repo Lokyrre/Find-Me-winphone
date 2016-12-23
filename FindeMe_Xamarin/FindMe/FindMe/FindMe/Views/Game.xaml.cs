12f572f69eb107f3b57de62c1cd6553673e05326
Jeremy <jeremy.paroux@gmail.com>
2016-12-23 12:29:48 +0100

    Ajout du chrono du jeux

--- FindeMe_Xamarin/FindMe/FindMe/FindMe/Views/Game.xaml.cs
+++ FindeMe_Xamarin/FindMe/FindMe/FindMe/Views/Game.xaml.cs
@@ -8,31 +8,35 @@ using System.Threading.Tasks;
 using Plugin.Vibrate;
 
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace FindMe.Views
 {
     public partial class Game : ContentPage
     {
         private int score;
+        private double addingTime;
+        private double coefficientTime ;
+        private int time;
+
         public Game(string gameType)
         {
             InitializeComponent();
             Settings.TypeGameSettings = gameType;
             GameViewModel.ClearListItem();
             score = 0;
+            addingTime = .2;
+            coefficientTime = 0.02;
+            time = 10000;
             Loading();
         }
 
-        async void Loading()
+        public async void Loading()
         {
             Random r = new Random();
             GameViewModel gvm = new GameViewModel();
             bool isWhiteIntruder = Convert.ToBoolean(r.Next(2));
             int intruder = r.Next(Settings.NbrIconSettings);
-            var progressBar = new ProgressBar
-            {
-                Progress = .2,
-            };
             for (int i = 0; i < Settings.NbrIconSettings; i++)
             {
                 var img = new Image();
@@ -53,6 +57,19 @@ namespace FindMe.Views
                         }
                         score += 150;
                         aLayout.Children.Clear();
+                        GameViewModel.ClearListItem();
+
+                        addingTime = addingTime - addingTime * coefficientTime;
+                        if (progress.Progress + addingTime > 1)
+                        {
+                            progress.Progress = 1;
+                            time = 10000;
+                        }
+                        else
+                        {
+                            progress.Progress = progress.Progress + addingTime;
+                            time = (int)( progress.Progress * 10000);
+                        }
                         Loading();
                     };
                 }
@@ -68,21 +85,24 @@ namespace FindMe.Views
                         }
                         score -= 33;
                         aLayout.Children.Clear();
+                        GameViewModel.ClearListItem();
                         Loading();
                     };
                 }
                 img.GestureRecognizers.Add(tapGestureRecognizer);
                 aLayout.Children.Add(img);
-                await progressBar.ProgressTo(.8, 250, Easing.Linear);
                 AbsoluteLayout.SetLayoutBounds(img, new Rectangle(1.0 * r.Next(100) / 100, 1.0 * r.Next(100) / 100, .1, .1));
                 AbsoluteLayout.SetLayoutFlags(img, AbsoluteLayoutFlags.All);
-
-
             }
 
-
+            labelScore.Text = "Score: " + score;
+            await progress.ProgressTo(0, (uint)time, Easing.Linear);
+            if(progress.Progress == 0)
+            {
+                await Navigation.PushAsync(new EndGame(score));
+            }
         }
 
     }
 }
-//GameViewModel.ClearListItem(); Navigation.PushAsync(new EndGame(score));
+//Navigation.PushAsync(new EndGame(score));
